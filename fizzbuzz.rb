8c8c9a7cf150c6528997a5e4d17c99dcc5772b7e
Arslan Ali <ali.arslan319@gmail.com>
2019-03-22 20:25:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-elsif int % 5 == 0 and int % 3 == 0
-  "FizzBuzz"
-  if int % 3 == 0
+  if int % 5 == 0 and int % 3 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
